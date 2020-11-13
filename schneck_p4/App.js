eae6d2f5f46383b66d352f6d2b01fbf573d4fc14
David Schneck <david.schneckster@hotmail.com>
2020-11-13 12:57:59 -0500

    Started work on each class

---
+++ schneck_p4/App.js
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+	static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Main Menu");
+		System.out.println("-----------");
+
+		System.out.println("1) create a new list");
+		System.out.println("2) load an existing list");
+		System.out.println("3) quit");
+		
+		int input = scanner.nextInt);
+
+		switch(input) {
+			case 1:
+				break;
+			case 2: 
+				break;
+			case 3: 
+				break;
+		}
+
+
+
+	}
+}
---
+++ schneck_p4/TaskList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList; 
+
+public class TaskList {
+	ArrayList<TaskItem> tasks  = new ArrayList<>();
+
+	public void  addTask(TaskItem toAdd) { // maybe it should be a reference
+
+
+	}
+	
+	public void removeTask(TaskItem toRemove) {
+	}
+	// TODO: decide how I want to format the input file
+	public loadTaskList() {
+		
+	}
+
+	
+
+
+}
